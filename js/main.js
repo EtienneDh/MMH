d7bafdaa0311987a234a3c384a678e1a3138d726
yanismdz <maoucha.yanis@gmail.com>
2016-09-06 09:44:15 +0200

    Did desktop navbar ansd styling with under menu in index.html and style.css

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(".menu-mobile-icon").click(function(){ // toggle the menu when clicking on mobile menu icon
+    $(".menu-list").fadeToggle(800);
+
+});
+
+if (window.matchMedia("(max-width: 700px)").matches) { //  when the size is under 700 px apply the click toggling to the undermenu mmh
+  
+$(".mmh").click(function(){
+    $(".under-menu-link").fadeToggle();
+});
+}
