21dde63beecbd1bc6d9bf18669c84b9c3909d624
Thomas Gossart <tg.59290@gmail.com>
2016-09-30 14:46:14 +0200

    made the triangle to moove according the mouse in main.js

--- js/main.js
+++ js/main.js
@@ -30,6 +30,35 @@ if (window.matchMedia("(max-width: 700px)").matches) { //  when the size is unde
         }
     });
 }
+
+// Move the triangle according to the current page
+
+// Get the left position on the active link in the menu
+var elementPosition = $('.active').offset();
+var elementLocation = elementPosition.left;
+
+// Get the width where to set the triangle
+var parentWidth = ($('.active').width())/2;
+
+// Set the triangle but substract the half of the tirangle size to be centered
+$('.triangle').css('left', ((elementLocation+parentWidth)-7.5));
+
+// When mouseover a menu title, the triangle follows
+$('.title').mouseenter(function(){
+
+  titlePosition = $(this).offset();
+  titleLocation = titlePosition.left;
+  titleWidth = ($(this).width())/2;
+
+  $('.triangle').css('left',((titleLocation+titleWidth)+12)).hide().fadeIn();
+});
+
+// When mouseleave returns to the original position
+$('.title').mouseleave(function(){
+  $('.triangle').css('left', ((elementLocation+parentWidth)-7.5));
+});
+
+
 /***************************** /Responsive Nav Bar *****************************/
 /********************************* Slider **************************************/
 /* Variables */
